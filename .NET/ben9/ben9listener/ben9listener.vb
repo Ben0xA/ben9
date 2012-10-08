b3e5f42769682241639f2bc93e65cb2a1ecf151f
Ben0xA <web@ben0xa.com>
2012-10-08 14:08:20 -0500

    Update to the .NET application. Changed default IP to 127.0.0.1, however for testing be sure to use your own IP.

--- .NET/ben9/ben9listener/ben9listener.vb
+++ .NET/ben9/ben9listener/ben9listener.vb
@@ -6,7 +6,7 @@ Public Module ben9listener
 
     Sub Main()
         Console.WriteLine("Starting server.")
-        srv.Listen("192.168.135.35", 4343)
+        srv.Listen("127.0.0.1", 4343)
         Console.ReadLine()
     End Sub
 
