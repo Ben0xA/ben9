8d023badcd8d83c55ed34d020750885d3dcaeb99
Ben0xA <web@ben0xa.com>
2012-10-09 10:25:50 -0500

    Adding error handling in case the connection failed.

--- ben9.py
+++ ben9.py
@@ -7,6 +7,7 @@ import os
 
 # create socket object
 xport = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+rslt = 0
 
 # send message to server; fail quietly if server is not running
 try:
@@ -14,11 +15,12 @@ try:
     # export in psv time|username|ip|machine_name
     msg = time.strftime("%m/%d/%Y %H:%M:%S",time.gmtime()) + '|' + getpass.getuser() + '|'
     msg += socket.gethostbyname(socket.gethostname()) + '|' + os.getenv('HOSTNAME')
-    xport.send(msg.encode())
+    rslt = xport.send(msg.encode())
 except:
     pass
 
 # close socket
-xport.shutdown(1)
-xport.close()
+if rslt != 0: # only close the socket if it was open to begin with
+    xport.shutdown(1)
+    xport.close()
 
