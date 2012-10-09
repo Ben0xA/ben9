8d99cb80d4f00aef7c5bd6c3697b6a81ccd8145e
Integgroll <integgroll@gmail.com>
2012-10-09 11:41:31 -0400

    Reworked the client to send less data, refactored some code, formatting changes, also isn't currently working in OSX because apparently they fubared the way sockets work

--- ben9.py
+++ ben9.py
@@ -3,20 +3,19 @@
 import socket
 import getpass
 import time
-import os
 
 # create socket object
 xport = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # send message to server; fail quietly if server is not running
 try:
-    xport.connect(('192.168.135.35',4444))
-    # export in psv time|username|ip|machine_name
-    msg = time.strftime("%m/%d/%Y %H:%M:%S",time.gmtime()) + '|' + getpass.getuser() + '|'
-    msg += socket.gethostbyname(socket.gethostname()) + '|' + os.getenv('HOSTNAME')
-    xport.send(msg.encode())
+  xport.connect(('localhost',4444))
+  xport.setblocking(0)
+  # export in psv time|username|ip|machine_name
+  msg = getpass.getuser()
+  xport.send(msg.encode())
 except:
-    pass
+  print "failcamping"
 
 # close socket
 xport.shutdown(1)
