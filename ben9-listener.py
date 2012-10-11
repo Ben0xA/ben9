1bdfcf7bfdb60cd983ea1fd510b9c73d9b247448
Ben0xA <web@ben0xa.com>
2012-10-11 08:53:47 -0500

    Added softkill of 'q' [enter] to stop the server instead of ctrl+c

--- ben9-listener.py
+++ ben9-listener.py
@@ -5,9 +5,22 @@ from sys import exit
 import socket
 import time
 import select
+import threading
+
+# variable declaration
+kp = ""
+timeout = 1
+
+def input():
+    global kp
+    kp = raw_input()
+    exit(1)
+
+thd = threading.Thread(target=input)
+thd.start()
+thd.join(2)
 
 # handle command-line options for ben9-listener
-
 usageString = "Usage: %prog [-l log_file]"
 parser = OptionParser(usage=usageString)
 
@@ -16,7 +29,6 @@ parser.add_option("-l", "--logfile", dest="logfile", metavar="LOGFILE", type="st
 (opts,args) = parser.parse_args()
 
 # open up the logfile, if user opts to use one
-
 if opts.logfile:
   try:
     logfile = open(opts.logfile,"a")
@@ -34,19 +46,23 @@ inport.bind(("localhost",4444))
 inport.setblocking(0)
 inport.listen(5)
 print "[*]Server started."
+print "[*]Type q [enter] to stop server."
 print "ServerTime|RemoteIP|RemoteName|ClientTime|ClientUser|ClientIP|ClientName"
 
 # wait for responses from ben9.py client
-while 7:
-  readers,writers,errors = select.select([inport],[],[])
-  for read in readers:
-    csocket, address = read.accept()
-    cltext = csocket.recv(300)
-    text = time.strftime("%m/%d/%Y %H:%M:%S",time.localtime()) + "|" 
-    text += address[0] + "|" + str(socket.gethostbyaddr(address[0])[0]) + "|"
-    text += cltext
-    
-    if opts.logfile:
-      logfile.write(text + "\n")
-    else:
-      print text
+while kp.lower() != 'q':
+  readers,writers,errors = select.select([inport],[],[], timeout)
+  if readers:      
+    for read in readers:
+      csocket, address = read.accept()
+      cltext = csocket.recv(300)
+      text = time.strftime("%m/%d/%Y %H:%M:%S",time.localtime()) + "|" 
+      text += address[0] + "|" + str(socket.gethostbyaddr(address[0])[0]) + "|"
+      text += cltext
+
+      if opts.logfile:
+        logfile.write(text + "\n")
+      else:
+        print text
+
+print "[*]Server stopped."
